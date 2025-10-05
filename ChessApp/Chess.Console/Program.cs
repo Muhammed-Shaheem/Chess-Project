debaaa281af43f1132554436336bb0f99c637a13
Muhammed Shaheem <Shaheemofcl@gmail.com>
2025-10-05 14:51:40 +0530

    made some code cleanup

--- ChessApp/Chess.Console/Program.cs
+++ ChessApp/Chess.Console/Program.cs
@@ -10,38 +10,19 @@ internal class Program
     {
         Utilities.InitializePieces(board);
         ConsoleUtilities.PrintChessBoard(board);
+        PlayGame();
 
+    }
+
+    private static void PlayGame()
+    {
         char turn = 'W';
         bool isWinOrTie = false;
 
         while (isWinOrTie == false)
         {
             (int fromRow, int fromCol, int toRow, int toCol, char pieceMoved) = ConsoleUtilities.UserInputValidation(board, turn);
-            bool isValidMove = false;
-            switch (pieceMoved)
-            {
-                case 'P':
-                    isValidMove = Pawn.Move(board, fromRow, fromCol, toRow, toCol);
-                    break;
-                case 'R':
-                    isValidMove = Rook.Move(board, fromRow, fromCol, toRow, toCol);
-                    break;
-                case 'N':
-                    isValidMove = Knight.Move(board, fromRow, fromCol, toRow, toCol);
-                    break;
-                case 'B':
-                    isValidMove = Bishop.Move(board, fromRow, fromCol, toRow, toCol);
-                    break;
-                case 'Q':
-                    isValidMove = Queen.Move(board, fromRow, fromCol, toRow, toCol);
-                    break;
-                case 'K':
-                    isValidMove = King.Move(board, fromRow, fromCol, toRow, toCol);
-                    break;
-
-                default:
-                    break;
-            }
+            bool isValidMove = IsValidMove(fromRow, fromCol, toRow, toCol, pieceMoved);
             if (isValidMove == false)
             {
                 Console.WriteLine("Invalid Move.");
@@ -57,10 +38,37 @@ internal class Program
             Console.WriteLine();
             ConsoleUtilities.PrintChessBoard(board);
         }
+    }
 
+    private static bool IsValidMove(int fromRow, int fromCol, int toRow, int toCol, char pieceMoved)
+    {
+        bool isValidMove = false;
+        switch (pieceMoved)
+        {
+            case 'P':
+                isValidMove = Pawn.Move(board, fromRow, fromCol, toRow, toCol);
+                break;
+            case 'R':
+                isValidMove = Rook.Move(board, fromRow, fromCol, toRow, toCol);
+                break;
+            case 'N':
+                isValidMove = Knight.Move(board, fromRow, fromCol, toRow, toCol);
+                break;
+            case 'B':
+                isValidMove = Bishop.Move(board, fromRow, fromCol, toRow, toCol);
+                break;
+            case 'Q':
+                isValidMove = Queen.Move(board, fromRow, fromCol, toRow, toCol);
+                break;
+            case 'K':
+                isValidMove = King.Move(board, fromRow, fromCol, toRow, toCol);
+                break;
 
+            default:
+                break;
+        }
 
-
+        return isValidMove;
     }
 
 
