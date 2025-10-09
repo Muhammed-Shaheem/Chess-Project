d76b2aef2e588885decd54c8be86bf37944f340b
Muhammed Shaheem <Shaheemofcl@gmail.com>
2025-10-09 15:38:42 +0530

    Added validation code for Knight move.

--- ChessApp/ChessLibrary/ChessPieces/Knight.cs
+++ ChessApp/ChessLibrary/ChessPieces/Knight.cs
@@ -4,29 +4,16 @@ public static class Knight
 {
     public static bool Move(string[,] board, int fromRow, int fromCol, int toRow, int toCol)
     {
+        if (board[toRow, toCol] != null && Utilities.IsSameColor(board[fromRow, fromCol], board[toRow, toCol]))
+        {
+            return false;
+        }
+
         if (board[fromRow, fromCol] == "BN")
         {
             if (board[toRow, toCol] == null)
             {
-                if (board[toRow, toCol] != null)
-                {
-                    // if the piece in the target is black or opponet king then return false
-                    char? firstChar = board[toRow, toCol]![0];
-                    if (firstChar == 'B')
-                    {
-                        return false;
-                    }
-                    else if (board[toRow, toCol] == "Wk")
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        Utilities.MovePiece(board, fromRow, fromCol, toRow, toCol, "BN");
-
-                        return true;
-                    }
-                }
+               
                 if (((toRow == fromRow + 2) && toCol == fromCol + 1 || toCol == fromCol - 1) || ((toRow == fromRow - 2) && toCol == fromCol + 1 || toCol == fromCol - 1))
                 {
                     Utilities.MovePiece(board, fromRow, fromCol, toRow, toCol, "BN");
@@ -34,10 +21,7 @@ public static class Knight
                 }
 
             }
-            else if (board[toRow, toCol] != null)
-            {
-
-            }
+     
         }
 
         if (board[fromRow, fromCol] == "WN")
