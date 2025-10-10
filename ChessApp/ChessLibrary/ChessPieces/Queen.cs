f9e5c13ffa2abfe3640d1fa43e78e8bd756873bb
Muhammed Shaheem <Shaheemofcl@gmail.com>
2025-10-10 12:12:44 +0530

    added basic move validation for queen

--- ChessApp/ChessLibrary/ChessPieces/Queen.cs
+++ ChessApp/ChessLibrary/ChessPieces/Queen.cs
@@ -5,6 +5,54 @@ public class Queen
 {
     public static bool Move(string[,] board, int fromRow, int fromCol, int toRow, int toCol)
     {
-        throw new NotImplementedException();
+        if (board[toRow, toCol] != null && Utilities.IsSameColor(board[fromRow, fromCol], board[toRow, toCol]))
+        {
+            return false;
+
+           
+        }
+
+        if (board[fromRow, fromCol] == "BQ")
+        {
+            if (((fromRow == toRow || fromCol == toCol) || (Math.Abs(fromRow - toRow) == Math.Abs(fromCol - toCol))) == false)
+            {
+                return false;
+            }
+
+
+            if (IsPathClear(board, fromRow, fromCol, toRow, toCol) == false)
+            {
+                return false;
+            }
+
+            Utilities.MovePiece(board, fromRow, fromCol, toRow, toCol, "BQ");
+            return true;
+
+
+
+        }
+
+        else if(board[fromRow, fromCol] == "WQ")
+        {
+            if (((fromRow == toRow || fromCol == toCol) || (Math.Abs(fromRow - toRow) == Math.Abs(fromCol - toCol))) == false)
+            {
+                return false;
+            }
+
+
+            if (IsPathClear(board, fromRow, fromCol, toRow, toCol) == false)
+            {
+                return false;
+            }
+
+            Utilities.MovePiece(board, fromRow, fromCol, toRow, toCol, "BQ");
+            return true;
+
+        }
+
+        return false;
     }
+
+
+
 }
